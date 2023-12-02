a1fcf7c0644593ff9c0b0bdb71a5c759d130fa7f
Patryk Palej <patrykpalej@gmail.com>
2023-12-02 14:44:06 +0100

    Modify project structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,16 @@
-import os
-import sys
 import streamlit as st
 from st_pages import Page, show_pages
-from dotenv import load_dotenv
-
-load_dotenv()
-sys.path.append(os.environ["PYTHONPATH"])
 
 from functions.data_loading import load_data_concurrently
 from functions.main_page import main_page
 
-os.chdir(os.environ["PYTHONPATH"])
 
 st.set_page_config(layout="wide", page_title="Real Estate Market Analysis")
 
 
 show_pages(
     [
-        Page("main.py", "Project description", "💻"),
+        Page("streamlit_app.py", "Project description", "💻"),
         Page("pages/page_houses.py", "Houses", "🏡"),
         Page("pages/page_lands.py", "Lands", "🌳"),
         Page("pages/page_apartments.py", "Apartments", "🏢")
